384e68520505a5588ea626caee7b0cfcf7089f82
IrinaMakarenko <rishana44@gmail.com>
2024-01-05 00:59:32 +0300

    Homework for Lesson_6

---
+++ src/main/java/org/example/AppData.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+public class AppData {
+    private String[] header;
+    private int[][] data;
+    public AppData() {
+    }
+    public String[] getHeader() {
+        return header;
+    }
+    public void setHeader(String[] header) {
+        this.header = header;
+    }
+    public int[][] getData() {
+        return data;
+    }
+    public void setData(int[][] data) {
+        this.data = data;
+    }
+    public void save(String filePath) throws IOException {
+        try (PrintWriter writer = new PrintWriter(new File(filePath))) {
+            // Записываем заголовок
+            for (int i = 0; i < header.length; i++) {
+                writer.print(header[i]);
+                if (i < header.length - 1) {
+                    writer.print(";");
+                }
+            }
+            writer.println();
+            // Записать данные
+            for (int[] row : data) {
+                for (int i = 0; i < row.length; i++) {
+                    writer.print(row[i]);
+                    if (i < row.length - 1) {
+                        writer.print(";");
+                    }
+                }
+                writer.println();
+            }
+        }
+    }
+    public void load(String filePath) throws IOException {
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            // Чтение заголовка
+            if (scanner.hasNextLine()) {
+                header = scanner.nextLine().split(";");
+            }
+            List<int[]> dataList = new ArrayList<>();
+            // Чтение данных
+            while (scanner.hasNextLine()) {
+                String[] line = scanner.nextLine().split(";");
+                int[] dataRow = new int[line.length];
+                for (int i = 0; i < line.length; i++) {
+                    dataRow[i] = Integer.parseInt(line[i]);
+                }
+                dataList.add(dataRow);
+            }
+            data = dataList.toArray(new int[dataList.size()][]);
+        }
+    }
+}
